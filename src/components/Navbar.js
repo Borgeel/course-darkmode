e8401c7e514310dae97db51f72029ea584678b38
Irfan <irfancesir93@gmail.com>
2022-06-06 15:03:25 +0200

    first commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import "../styles.css";
+
+const Navbar = (props) => {
+  return (
+    <nav>
+      <h1>ReactFacts</h1>
+      <h2>React Course - Project 1</h2>
+    </nav>
+  );
+};
+export default Navbar;
